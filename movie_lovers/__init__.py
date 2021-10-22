170a02ef13b11167397cb9396d5ea244d7da9c9b
Vahideh Ghanbari <vahideh.gh@gmail.com>
2021-10-21 21:27:09 -0700

    Create CRUD for movie_lovers API and add helpers.py

--- movie_lovers/__init__.py
+++ movie_lovers/__init__.py
@@ -4,7 +4,8 @@ from .authentication.routes import auth
 from .api.routes import api
 from .site.routes import site
 from flask_migrate import Migrate
-from .models import db, login_manager
+from .models import db, login_manager, ma
+from flask_cors import CORS
 
 # instantiating a new flask app
 app = Flask(__name__)
@@ -19,4 +20,8 @@ db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'auth.signin'
 
+ma.init_app(app)
+
 migrate = Migrate(app, db)
+
+CORS(app)
--- movie_lovers/models.py
+++ movie_lovers/models.py
@@ -5,11 +5,12 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 from flask_login import LoginManager, UserMixin
-
+from flask_marshmallow import Marshmallow
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+ma = Marshmallow()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -23,6 +24,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable = False)
     token = db.Column(db.String, unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    movie = db.relationship('Movie', backref = 'owner', lazy = True)
 
     def __init__(self, email, password, token = '', id = ''):
         self.id = self.set_id()
@@ -41,3 +43,30 @@ class User(db.Model, UserMixin):
     
     def set_token(self, lenght):
         return secrets.token_hex(lenght)
+
+
+class Movie(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String(150))
+    genre = db.Column(db.String(100))
+    year = db.Column(db.String(200))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+
+    def __init__(self, name, genre, year, user_token, id = ''):
+        self.id = self.set_id()
+        self.name = name
+        self.genre = genre
+        self.year = year
+        self.user_token = user_token
+
+    def set_id(self):
+        return (secrets.token_urlsafe())
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'name','genre', 'year']
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
